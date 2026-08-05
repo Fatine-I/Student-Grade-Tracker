a29d953f3bca61b00d6c5cc45056fada3b1fe3f2
Fatine-I <f.icyimpaye@alustudent.com>
2026-08-05 21:04:33 +0400

    commit change 1 to init_constructor file

---
+++ init_constructor.py
@@ -0,0 +1,15 @@
+class Assignment:
+    def __init__(self, subject,title,assignment_type,score,max_score,due_date):#.....constructor method for class Assignment objects
+        self.subject=subject
+        self.title=title
+        self.assignment_type=assignment_type
+        self.score=score
+        self.max_score=max_score
+    def add_assignment(self, subject,title,assignment_type,score):#..................recording a new assignment in existing course, subject and title
+        pass
+    def display_assignment(self):#...................................................display contents of added assignment only
+        pass
+    def list_assignment(self):#......................................................listing the added assignments if requested in the main function
+        pass
+    def filter_assignment(self):#....................................................filterning assignments and displaying what is requested
+        pass
