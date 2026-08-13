ade5db528998a975b0d94a7b9a8a56847c6cb617
Fatine-I <f.icyimpaye@alustudent.com>
2026-08-13 17:56:30 +0400

    commit changes2 to Exam class file

--- Exam_class.py
+++ Exam_class.py
@@ -1,6 +1,6 @@
 class Exam(Assignment):
     def __init__(self,subject,title,score,max_score,due_date):
-        super().__init__(subject,title,score,max_score,due_date,"exam")
+        super().__init__(subject,title,score,max_score,due_date,"Exam")
         self.exam_list = []
 
     def add_exam(self, subject, title, due_date, max_score, graded=False, score=None):
