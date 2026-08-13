89e5b6aae4bf7954498006cad211d0c862b0682f
Fatine-I <f.icyimpaye@alustudent.com>
2026-08-13 15:11:06 +0400

    commit changes1 to Exam class file

--- Exam_class.py
+++ Exam_class.py
@@ -1,3 +1,14 @@
-class Exam:
-    def __int__(self,subject,title):
-        pass
+class Exam(Assignment):
+    def __init__(self,subject,title,score,max_score,due_date):
+        super().__init__(subject,title,score,max_score,due_date,"exam")
+        self.exam_list = []
+
+    def add_exam(self, subject, title, due_date, max_score, graded=False, score=None):
+        self.exam_list.append({
+            "subject": subject,
+            "title": title,
+            "due_date": due_date,
+            "max_score": max_score,
+            "grade": graded,
+            "scores": [] if score is None else [score]
+        })
