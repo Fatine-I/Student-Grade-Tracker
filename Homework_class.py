be8abe63501567b81036443980b11586cecc30ed
Fatine-I <f.icyimpaye@alustudent.com>
2026-08-13 14:11:02 +0400

    commit changes1 to Homework class file

--- Homework_class.py
+++ Homework_class.py
@@ -1,3 +1,14 @@
-class Homework:
-    def __init__(self,subject,title):
-        pass
+class Homework(Assignment):
+    def __init__(self,subject,title,score,max_score,due_date):
+        super().__init__(subject,title,score,max_score,due_date,"Homework")
+        self.homework_list = []
+
+    def add_homework(self, subject, title, due_date, max_score, graded=False,score=None):
+        self.homework_list.append({
+            "subject": subject,
+            "title": title,
+            "due_date": due_date,
+            "max_score": max_score,
+            "graded": graded,
+            "scores": [] if score is None else [score]
+        })
