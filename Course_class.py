0bdf34c55b3a2c6b5535483669234574b440ccd6
Fatine-I <f.icyimpaye@alustudent.com>
2026-08-14 09:49:12 +0400

    Commit changes2 to Course class file

--- Course_class.py
+++ Course_class.py
@@ -1,6 +1,5 @@
 class Course:
     def __init__(self):
-        subjects=self.course.keys()
         self.course={
             'Math':('Algebra','Geometry','Trigonometry'),
             'Physics':('Thermodynamics', 'Mechanics', 'Electromagnetism'),
