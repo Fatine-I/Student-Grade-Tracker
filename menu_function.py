8917506a6166e362f288a8a8d3761b0026ad82dd
Fatine-I <f.icyimpaye@alustudent.com>
2026-08-14 09:45:08 +0400

    Commit changes2 to main function file

--- menu_function.py
+++ menu_function.py
@@ -8,31 +8,27 @@ def main():
         print("4.Filter assignments")
         print("5. Show summary")
         print("6. Exit")
-    choice=input("\nChoose an option: ").strip()
-    if choice=="1":
-        choice2=input("1. Add homework\n2.Press Enter to return to the main menu").strip()
-        while choice2=="1":
-            tracker.add_homework()
-        else: 
+        choice=input("\nChoose an option: ").strip()
+        if choice=="1":
+            tracker.add_homework() 
             input("\nPress Enter to return to the main menu")
-    elif choice == "2":
-        choice2=input("1. Add homework\n2.Press Enter to return to the main menu").strip()
-        while choice2=="1":
+        elif choice == "2":
             tracker.add_exam()
+            input("\nPress Enter to return to the main menu")
+        elif choice=="3":
+            tracker.list_assignments()
+            input("\nPress Enter to return to the main menu")
+        elif choice=="4":
+            tracker.filter_assignments()
+            input("\nPress Enter to return to the main menu")
+        elif choice=="5":
+            tracker.summary()
+            input("\nPress Enter to return to the main menu")
+        elif choice=="6":
+            break
         else:
+            print("\nInvalid choice. Please choose 1-6")
             input("\nPress Enter to return to the main menu")
 
-    elif choice=="3":
-        tracker.list_assignment()
-        input("\nPress Enter to return to the main menu")
-    elif choice=="4":
-        tracker.filter_assignment()
-        input("\nPress Enter to return to the main menu")
-    elif choice=="5":
-        tracker.summary()
-        input("\nPress Enter to return to the main menu")
-    elif choice=="6":
-        exit()
-    else:
-        print("\nInvalid choice. Please choose 1-6")
-        input("\nPress Enter to return to the main menu")
+if __name__=="__main__": 
+    main()
