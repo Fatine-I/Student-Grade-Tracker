829467e8c87723552063afc905e2faa6a54bd683
Fatine-I <f.icyimpaye@alustudent.com>
2026-08-14 09:48:01 +0400

    Commit changes3 to Assigment class file

--- Assignment_class.py
+++ Assignment_class.py
@@ -1,7 +1,8 @@
 class Assignment(Course):
-    def __init__(self, subject,title,assignment_type,score,max_score,due_date):#.....constructor method for class Assignment objects
+    def __init__(self, subject,title,score,max_score,due_date,assignment_type):#.....constructor method for class Assignment objects
         self.subject=subject
         self.title=title
         self.score=score
         self.max_score=max_score
-        self.due_date=datetime.datetime.strptime('%Y-%m-%d %H:%M:%S').date()
+        self.due_date=due_date
+        self.assignment_type=assignment_type
