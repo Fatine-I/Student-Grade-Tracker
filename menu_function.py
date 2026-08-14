bc83072a94e35cfcb655aa4743884f1ed040ba7e
Fatine-I <f.icyimpaye@alustudent.com>
2026-08-14 21:39:56 +0400

    Commit the real menu interface code lines in menu_function file

--- menu_function.py
+++ menu_function.py
@@ -1,8 +1,36 @@
 #Initial setup tested with a working print statement
-print("Grade Tracker setup successful!")
-
 def main():
-    print("Grade Tracker is running!")
+    tracker=GradeTracker()
+    while True:
+        print("GRADE TRACKER\n\n\n")
+        print("1. Add homework")
+        print("2. Add exam")
+        print("3. List assignments")
+        print("4.Filter assignments")
+        print("5. Show summary")
+        print("6. Exit")
+        choice=input("\nChoose an option: ").strip()
+        if choice=="1":
+            tracker.add_homework() 
+            input("\nPress Enter to return to the main menu")
+        elif choice == "2":
+            tracker.add_exam()
+            input("\nPress Enter to return to the main menu")
+        elif choice=="3":
+            tracker.list_assignments()
+            input("\nPress Enter to return to the main menu")
+        elif choice=="4":
+            tracker.filter_assignments()
+            input("\nPress Enter to return to the main menu")
+        elif choice=="5":
+            tracker.summary()
+            input("\nPress Enter to return to the main menu")
+        elif choice=="6":
+            break
+        else:
+            print("\nInvalid choice. Please choose 1-6")
+            input("\nPress Enter to return to the main menu")
 
-if __name__ == "__main__":
+if __name__=="__main__":
+    print("Initial setup test: program started successfully.") 
     main()
