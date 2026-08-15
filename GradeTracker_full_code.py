5790148239f5fcd5ddb3361f9d1b0c8cad5b12e8
Fatine-I <f.icyimpaye@alustudent.com>
2026-08-15 08:23:48 +0400

    Append Assignment class to grade tracker full code file

--- GradeTracker_full_code.py
+++ GradeTracker_full_code.py
@@ -9,4 +9,11 @@ class Course:
             'Computer_Science':('Computer Architecture','Software Applications','Programming')
         }
 
-        
+        class Assignment(Course):
+    def __init__(self, subject,title,score,max_score,due_date,assignment_type):#.....constructor method for class Assignment objects
+        self.subject=subject
+        self.title=title
+        self.score=score
+        self.max_score=max_score
+        self.due_date=due_date
+        self.assignment_type=assignment_type
