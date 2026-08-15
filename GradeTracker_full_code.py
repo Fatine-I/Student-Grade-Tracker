f674a2c1219061f815abe803270af044ecfe929a
Fatine-I <f.icyimpaye@alustudent.com>
2026-08-15 08:08:02 +0400

    Append Course class to Grade Tracker full code file

--- GradeTracker_full_code.py
+++ GradeTracker_full_code.py
@@ -1,2 +1,8 @@
 import time
-import datetime from datetime
+import datetime from datetimeclass Course:
+    def __init__(self):
+        self.course={
+            'Math':('Algebra','Geometry','Trigonometry'),
+            'Physics':('Thermodynamics', 'Mechanics', 'Electromagnetism'),
+            'Computer_Science':('Computer Architecture','Software Applications','Programming')
+        }
