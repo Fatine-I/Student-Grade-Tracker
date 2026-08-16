e97e2f3aebd18c4f2390c30badffbdb019b4652b
Fatine-I <f.icyimpaye@alustudent.com>
2026-08-16 07:56:33 +0400

    Append Homework class to grade tracker full code file

--- GradeTracker_full_code.py
+++ GradeTracker_full_code.py
@@ -33,3 +33,17 @@ class Exam(Assignment):
             "scores": [] if score is None else [score]
         })
 
+class Homework(Assignment):
+    def __init__(self,subject,title,score,max_score,due_date):
+        super().__init__(subject,title,score,max_score,due_date,"Homework")
+        self.homework_list = []
+
+    def add_homework(self, subject, title, due_date, max_score, graded=False,score=None):
+        self.homework_list.append({
+            "subject": subject,
+            "title": title,
+            "due_date": due_date,
+            "max_score": max_score,
+            "graded": graded,
+            "scores": [] if score is None else [score]
+        })
