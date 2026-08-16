7c5aff520a2b20e59abb89f7ff3cceac535cdd1a
Fatine-I <f.icyimpaye@alustudent.com>
2026-08-16 07:53:05 +0400

    Append Exam class to grade tracker full code file

--- GradeTracker_full_code.py
+++ GradeTracker_full_code.py
@@ -18,3 +18,17 @@ class Assignment(Course):
         self.due_date=due_date
         self.assignment_type=assignment_type
 
+class Exam(Assignment):
+    def __init__(self,subject,title,score,max_score,due_date):
+        super().__init__(subject,title,score,max_score,due_date,"Exam")
+        self.exam_list = []
+
+    def add_exam(self, subject, title, due_date, max_score, graded=False, score=None):
+        self.exam_list.append({
+            "subject": subject,
+            "title": title,
+            "due_date": due_date,
+            "max_score": max_score,
+            "grade": graded,
+            "scores": [] if score is None else [score]
+        })
